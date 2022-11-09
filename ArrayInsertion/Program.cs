e0740536e296ddbd8fa831a36abf9a5faaccbcad
Nayana Ramesh <107012@tfcorp.com>
2022-11-09 19:28:10 +0530

    array insertion added

---
+++ ArrayInsertion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayInsertion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Insertion3 insertion3 = new Insertion3();
+            insertion3.Array3();
+        }
+    }
+}
